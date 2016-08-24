49cb1ba48855740401a4f41b03bba7879e4f848c
hideakis <hideaki.s621@gmail.com>
2016-08-24 17:07:44 +0900

    Initial Commit

---
+++ SwiftFunctions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFunctions
+//
+//  Created by Suzuki Hideaki on 2016/08/24.
+//  Copyright © 2016年 Suzuki Hideaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
