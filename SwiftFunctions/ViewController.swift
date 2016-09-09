bea927f0ac6bab273b77eec59de0182ec0fe9f3e
hideakis <hideaki.s621@gmail.com>
2016-09-09 09:45:31 +0900

    Learn about OpenSafari

--- SwiftFunctions/ViewController.swift
+++ SwiftFunctions/ViewController.swift
@@ -7,21 +7,56 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+var urlString:String = "https://google.com"
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		Log(String(getLastMonthOfDay()))
+		let button = UIButton(frame: CGRect(x: 10, y: 10, width: 160, height: 50))
+		button.center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
+		button.setTitle("OpenSafari", forState: .Normal)
+		button.addTarget(self, action: #selector(self.openSafari), forControlEvents: .TouchUpInside)
+		button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+		self.view.addSubview(button)
 
+	}
+
+	func openSafari() {
+		if #available(iOS 9.0, *) {
+			let _url:NSURL = NSURL(string: urlString)!
+			let _brow = SFSafariViewController(URL: _url, entersReaderIfAvailable: true)
+			presentViewController(_brow, animated: true, completion: nil)
+			_brow.delegate = self
+		} else {
+			// Fallback on earlier versions
+			let _alert = UIAlertView()
+			_alert.title = "SFSafariViewController is iOS9 over."
+			_alert.addButtonWithTitle("OK")
+			_alert.show()
+		}
+	}
+
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
+	// SafariViewController Delegate Method
+	@available(iOS 9.0, *)
+	func safariViewController(controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+		print("safariview:didCompleteInitialLoad")
+	}
 
+	@available(iOS 9.0, *)
+	func safariViewControllerDidFinish(controller: SFSafariViewController) {
+		print("safariview:closeSafari")
+	}
 }
 
